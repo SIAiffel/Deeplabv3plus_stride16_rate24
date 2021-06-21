8e3d93e56335065b54a445f82656408d6fb31533
Isaac Kim <kimisaac0307@gmail.com>
2021-06-21 15:29:47 +0900

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,7 @@
+from .voc import VOCSegmentation
+from .cityscapes import Cityscapes
+from .satellites_building import Satellites_building
+from .satellites_road import Satellites_road
+from .satellites import Satellites
+from .satellites_multi import Satellites_multi
+from .cityscapes2 import Cityscapes2
